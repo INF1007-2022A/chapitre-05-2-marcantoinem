43631f0807970b7080d7aaa3f5ac5d46d54848dc
lemark <marcantoine_manningham.com>
2022-09-21 13:42:26 -0400

    exercice

--- exercice.py
+++ exercice.py
@@ -4,18 +4,32 @@
 import random
 
 def get_bill(name, data):
-	INDEX_NAME = 0
-	INDEX_QUANTITY = 1
-	INDEX_PRICE = 2
+	cost = 0
+	for _, item_qt, price in data:
+		cost += item_qt*price
 
-	return ""
+	print(name)
+	print("SOUS TOTAL    {0:.2f} $".format(cost))
+	print("TAXES         {0:.2f} $".format(cost * 0.15))
+	print("TOTAL         {0:.2f} $".format(cost * 1.15))
 
 
 def format_number(number, num_decimal_digits):
-	return ""
+	return "{0:.{1}f}".format(number, num_decimal_digits)
 
 def get_triangle(num_rows):
-	return ""
+	total_size = num_rows * 2 + 1
+	triangle = "".join("+" for i in range(total_size)) + "\n"
+	for row in range(num_rows):
+		triangle += "+"
+		space = num_rows - row - 1
+		triangle += "".join(" " for i in range(space))
+		a = total_size - 2*space - 2
+		triangle += "".join("A" for i in range(a))
+		triangle += "".join(" " for i in range(space))
+		triangle += "+\n"
+	triangle += "".join("+" for i in range(total_size))
+	return triangle
 
 
 if __name__ == "__main__":
